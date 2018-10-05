300782ac29bb94b1c813efb895a2c1585aef1397
birolemekli <43793262+birolemekli@users.noreply.github.com>
2018-10-05 16:24:40 +0300

    Update pinglog.py

--- pinglog.py
+++ pinglog.py
@@ -4,9 +4,7 @@ import socket
 import struct
 from ctypes import *
 import time
-
 zmn = time.strftime("%d/%m/%Y  %H:%M:%S")
-
 class IP(Structure):
     _fields_ = [
         ("ihl", c_ubyte, 4),
@@ -21,11 +19,9 @@ class IP(Structure):
         ("src", c_uint32),
         ("dst", c_uint32)
     ]
-
     def __new__(self, socket_buffer=None):
         return self.from_buffer_copy(socket_buffer)
     def __init__(self, socket_buffer=None):
-
         self.protocol_map = {
             1: "ICMP"
         }
@@ -37,8 +33,6 @@ class IP(Structure):
         self.ip_dst = socket.inet_ntoa(struct.pack("<L", self.dst))
 data = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
 data.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
-
-
 try:
     while True:
         zmn = time.strftime("%d/%m/%Y  %H:%M:%S")
@@ -47,7 +41,6 @@ try:
         ip_header = IP(ip_buffer)
         dosya=open("log","a")
         dosya.write(ip_header.protocol+"  Target="+ip_header.ip_src+"  Source="+ip_header.ip_dst+"  Time="+zmn+"\n")
-        print({ip_header.protocol},  {ip_header.ip_src},"-->", {ip_header.ip_dst})
 except KeyboardInterrupt:
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
